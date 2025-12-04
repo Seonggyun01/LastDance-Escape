0dcb6a4b00df5c7b5db93f94c89ca29ba9e47409
Seonggyun01 <thdrbs010314@naver.com>
2025-12-04 13:28:01 +0900

    feat(playRecord): PlayRecord 엔티티 추가 및 생성 검증 로직 구현
    - Member, Theme와 N:1 연관관계 매핑 추가
    - durationSec, playDate, hintCount, 필드 정의
    - 생성자에 검증 메서드 추가하여 검증
    - 유효하지 않은 값일 경우 IllegalArgumentException 추가

--- src/main/java/com/lastdance/escape/playRecord/PlayRecord.java
+++ src/main/java/com/lastdance/escape/playRecord/PlayRecord.java
@@ -1,4 +1,86 @@
 package com.lastdance.escape.playRecord;
 
+import com.lastdance.escape.member.Member;
+import com.lastdance.escape.problem.Problem;
+import com.lastdance.escape.theme.Theme;
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import lombok.Getter;
+
+@Entity
+@Getter
 public class PlayRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private int durationSec;
+
+    @Column(nullable = false)
+    private LocalDate playDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "theme_id", nullable = false)
+    private Theme theme;
+
+    @Column(nullable = false)
+    private int hintCount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
+    protected PlayRecord() {
+    }
+
+    public PlayRecord(int durationSec, LocalDate playDate, Theme theme,
+                      int hintCount, Member member) {
+        validate(durationSec, playDate, theme, hintCount, member);
+        this.durationSec = durationSec;
+        this.playDate = playDate;
+        this.theme = theme;
+        this.hintCount = hintCount;
+        this.member = member;
+    }
+
+    private void validate(int durationSec, LocalDate playDate, Theme theme,
+                          int hintCount, Member member) {
+        validateDurationSec(durationSec);
+        validatePlayDate(playDate);
+        validateTheme(theme);
+        validateHintCount(hintCount);
+        validateMember(member);
+    }
+
+    private void validateMember(Member member) {
+        if(member == null){
+            throw new IllegalStateException("회원 정보는 반드시 필요합니다.");
+        }
+    }
+
+    private void validateHintCount(int hintCount) {
+        if (hintCount < 0) {
+            throw new IllegalStateException("힌트 사용 개수 0이상이어야 합니다. 입력된 힌트 개수 = " + hintCount);
+        }
+    }
+
+    private void validateTheme(Theme theme) {
+        if(theme == null){
+            throw new IllegalStateException("테마 정보는 반드시 필요합니다.");
+        }
+    }
+
+    private void validatePlayDate(LocalDate playDate) {
+        if(playDate == null){
+            throw new IllegalStateException("진행 날짜는 비어있을 수 없습니다.");
+        }
+    }
+
+    private void validateDurationSec(int durationSec) {
+        if(durationSec < 0){
+            throw new IllegalStateException("진행 시간은 0초 이상이어야 합니다.");
+        }
+    }
 }
